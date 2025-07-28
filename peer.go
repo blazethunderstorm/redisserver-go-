d10f896a56dcd2d3a9048c22a76c7e3eb0c92fff
ANIRUDH <anirudhnarang0@gmail.com>
2025-07-28 21:13:11 +0530

    added listener

--- peer.go
+++ peer.go
@@ -1,6 +1,12 @@
 package main
 
-import "net"
+import (
+	"io"
+	"net"
+    "log"
+	"fmt"
+	"github.com/tidwall/resp"
+)
 
 type Peer struct {
 	conn  net.Conn
@@ -18,4 +24,50 @@ func NewPeer(conn net.Conn, msgCh chan Message, delCh chan *Peer) *Peer {
 		msgCh: msgCh,
 		delCh: delCh,
 	}
+}
+
+
+func (p *Peer) readLoop() error {
+	rd := resp.NewReader(p.conn)
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			p.delCh <- p
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var cmd Command
+		if v.Type() == resp.Array {
+			rawCMD := v.Array()[0]
+			switch rawCMD.String() {
+			case CommandClient:
+				cmd = ClientCommand{
+					value: v.Array()[1].String(),
+				}
+			case CommandGET:
+				cmd = GetCommand{
+					key: v.Array()[1].Bytes(),
+				}
+			case CommandSET:
+				cmd = SetCommand{
+					key: v.Array()[1].Bytes(),
+					val: v.Array()[2].Bytes(),
+				}
+			case CommandHELLO:
+				cmd = HelloCommand{
+					value: v.Array()[1].String(),
+				}
+			default:
+				fmt.Println("got this unhandled command", rawCMD)
+			}
+			p.msgCh <- Message{
+				cmd:  cmd,
+				peer: p,
+			}
+		}
+	}
+	return nil
 }
